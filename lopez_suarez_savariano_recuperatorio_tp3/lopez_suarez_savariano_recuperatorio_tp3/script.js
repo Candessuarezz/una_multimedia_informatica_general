d03845385719e6e2dfe534dfada24ce91198d755
Candessuarezz <147281572+Candessuarezz@users.noreply.github.com>
2025-11-22 00:05:29 -0300

    lopez_suarez_savariano_recuperatorio_tp3

---
+++ lopez_suarez_savariano_recuperatorio_tp3/lopez_suarez_savariano_recuperatorio_tp3/script.js
@@ -0,0 +1,54 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  function mostrar(id) {
+    const artistasContainer = document.querySelector('.artistas-container');
+    if (artistasContainer) artistasContainer.style.display = 'none';
+
+    const secciones = document.querySelectorAll('.contenido');
+    secciones.forEach(sec => sec.style.display = 'none');
+
+    const seleccionada = document.getElementById(id);
+    if (seleccionada) {
+      seleccionada.style.display = 'block';
+    }
+  }
+  window.mostrar = mostrar;
+
+  function volver() {
+    location.reload();
+  }
+  window.volver = volver;
+
+  // --- ABRIR IMAGEN ---
+  function abrirImagen(url) {
+    const visor = document.getElementById("visor");
+    const grande = document.getElementById("img-grande");
+
+    grande.src = url;
+    visor.style.display = "flex";
+  }
+  window.abrirImagen = abrirImagen;
+
+  // --- CERRAR AL HACER CLICK FUERA ---
+  const visor = document.getElementById("visor");
+  if (visor) {
+    visor.addEventListener("click", (e) => {
+      if (e.target === visor) {
+        visor.style.display = "none";
+      }
+    });
+  }
+
+  // formulario
+  const form = document.getElementById('form-contacto');
+  const mensaje = document.getElementById('mensaje-enviado');
+
+  if (form) {
+    form.addEventListener('submit', function(event) {
+      event.preventDefault();
+      if (mensaje) mensaje.textContent = '¡Enviado con éxito!';
+      form.reset();
+    });
+  }
+
+});
